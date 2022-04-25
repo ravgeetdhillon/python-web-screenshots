95e931395f62c25e032f3d367f5ed14c06c1fde2
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2022-04-25 15:10:34 +0530

    fix: updated URLs

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://www.ravsam.in')
-driver.save_screenshot('my_screenshot.png')
+driver.get('https://urlbox.io/')
+driver.save_screenshot('screenshot.png')
 driver.quit()
 
 
 options = Options()
 options.headless = True
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-driver.get("https://news.ycombinator.com/")
-driver.save_screenshot('hn_homepage.png')
+driver.get("https://urlbox.io/")
+driver.save_screenshot('screenshot.png')
 driver.quit()
 
 
-imgkit.from_url('https://youtube.com', 'youtube.png')
+imgkit.from_url('https://urlbox.io/', 'screenshot.png')
 
 
 load_dotenv()
@@ -30,7 +30,14 @@ API_KEY = os.getenv('API_KEY')
 API_SECRET = os.getenv('API_SECRET')
 
 urlbox_client = UrlboxClient(api_key=API_KEY, api_secret=API_SECRET)
-response = urlbox_client.get({"url": "https://urlbox.io/"})
+
+response = urlbox_client.get({
+    "url": "https://urlbox.io/",
+    "format": "jpg",
+    "full_page": False,
+    "hide_cookie_banners": True,
+    "block_ads": True
+})
 
 with open("screenshot.png", "wb") as f:
     f.write(response.content)
